cfb1531f492a05251d765d18547403eda7c546be
Andrey Dyachkov <andrey.dyachkov@gmail.com>
2017-10-17 23:58:23 +0200

    minor fixes

--- hardware/temp_sensor/temp_sensor.ino
+++ hardware/temp_sensor/temp_sensor.ino
@@ -2,18 +2,30 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include "settings.h"
-#include "DHT.h"
-#define DHTPIN D4
-#define DHTTYPE DHT22
-DHT dht(DHTPIN, DHTTYPE);
+
+#include "PietteTech_DHT.h"
+
+// system defines
+#define DHTTYPE  DHT22           // Sensor type DHT11/21/22/AM2301/AM2302
+#define DHTPIN   D4              // Digital pin for communications
 #define DHT_PWR_PIN D8
 // sleep for this many seconds
 const int sleepSeconds = 600;
 
-float humidity, temperature, heatIndex;
-char str_humidity[10], str_temperature[10], str_heatIndex[10];
+float humidity, temperature, dewPoint;
+char str_humidity[10], str_temperature[10], str_dewPoint[10];
+
+//declaration
+void dht_wrapper(); // must be declared before the lib initialization
+
+// Lib instantiate
+PietteTech_DHT DHT(DHTPIN, DHTTYPE, dht_wrapper);
 
 ADC_MODE(ADC_VCC);
+
+void dht_wrapper() {
+  DHT.isrCallback();
+}
 
 void setup() {
   Serial.begin(9600);
@@ -21,17 +33,13 @@ void setup() {
   // Connect D0 to RST to wake up
   pinMode(D0, WAKEUP_PULLUP);
   pinMode(DHT_PWR_PIN, OUTPUT);
-  digitalWrite(DHT_PWR_PIN, HIGH);
-  delay(2000);
     
   fillSensorData();
   connectWifi();
   String deviceId = macAddress();
   String vcc = readVcc();
   post("/home/weather/status", 
-  "{\"deviceId\":\""+deviceId+"\",\"humidity\":"+str_humidity+",\"temperature\":"+str_temperature+",\"heatIndex\":"+str_heatIndex+",\"vcc\":"+vcc+"}");
-
-  digitalWrite(DHT_PWR_PIN, LOW);
+  "{\"deviceId\":\""+deviceId+"\",\"humidity\":"+str_humidity+",\"temperature\":"+str_temperature+",\"heatIndex\":"+str_dewPoint+",\"vcc\":"+vcc+"}");
   
   // convert to microseconds
   ESP.deepSleep(sleepSeconds * 1000000);
@@ -41,31 +49,52 @@ void loop() {
 }
 
 void fillSensorData() {
-  dht.begin();
-  delay(3000);
+  digitalWrite(DHT_PWR_PIN, HIGH);
+  delay(2000);
 
-  float humidity = dht.readHumidity();
-  float temperature = dht.readTemperature();
-  if (isnan(humidity) || isnan(temperature)) {
-    Serial.println("Failed to read from DHT sensor! restarting device");
-    ESP.restart();
+  int attempts = 0;
+  while(attempts < 3) {
+    Serial.println("Attempt to get DHT data");
+    int acquireresult = DHT.acquireAndWait(1000);
+    if (acquireresult == 0) {
+      temperature = DHT.getCelsius();
+      humidity = DHT.getHumidity();
+      dewPoint = DHT.getDewPoint();
+      break;
+    } else {
+      temperature = humidity = dewPoint = 0;
+    }
+
+    attempts++;
+    delay(500);
   }
-  float heatIndex = dht.computeHeatIndex(temperature, humidity, false);
+  DHT.reset();
+  Serial.print("temperature: ");
+  Serial.println(temperature);
+
+  digitalWrite(DHT_PWR_PIN, LOW);
+  delay(1000);
 
   // Convert the floats to strings and round to 2 decimal places
   dtostrf(humidity, 1, 2, str_humidity);
   dtostrf(temperature, 1, 2, str_temperature);
-  dtostrf(heatIndex, 1, 2, str_heatIndex);
+  dtostrf(dewPoint, 1, 2, str_dewPoint);
 }
 
 void connectWifi() {
+  Serial.print("Wifi creds: ");
+  Serial.print(ssid);
+  Serial.println(password);
+
   WiFi.begin(ssid, password);
   int wifiConnectAttempts = 0;
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
     if (wifiConnectAttempts > 20) {
-      Serial.print("Made 10 attempts, restarting device");
+      Serial.println("Made 20 attempts, restarting device");
+      Serial.print("Wifi status: ");
+      Serial.println(WiFi.status());
       ESP.restart();
     }
     wifiConnectAttempts++;
